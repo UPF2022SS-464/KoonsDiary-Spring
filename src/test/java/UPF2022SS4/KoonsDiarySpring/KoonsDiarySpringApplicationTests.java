1bca06d3d83419ad2f3ca275935f0820b4adc138
ingkoon <ing97220@naver.com>
2022-03-17 02:41:18 +0900

    KDS-3 프로젝트 수정

---
+++ src/test/java/UPF2022SS4/KoonsDiarySpring/KoonsDiarySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package UPF2022SS4.KoonsDiarySpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoonsDiarySpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
