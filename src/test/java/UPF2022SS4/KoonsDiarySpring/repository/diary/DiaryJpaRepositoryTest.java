ef6cc6ba394e8613b29ba61f66c3482f320d9d31
ingkoon <ing97220@naver.com>
2022-04-07 14:55:08 +0900

    testcode 추가

---
+++ src/test/java/UPF2022SS4/KoonsDiarySpring/repository/diary/DiaryJpaRepositoryTest.java
@@ -0,0 +1,13 @@
+package UPF2022SS4.KoonsDiarySpring.repository.diary;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+class DiaryJpaRepositoryTest {
+
+}
