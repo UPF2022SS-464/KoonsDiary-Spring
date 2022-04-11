e0365b3bebef714298c233b75ee1421b6026a656
ingkoon <ing97220@naver.com>
2022-04-11 16:35:19 +0900

    KDS-46 S3 bucket to EC2 환경설정

---
+++ src/main/java/UPF2022SS/KoonsDiarySpring/api/controller/ProfileController.java
@@ -0,0 +1,31 @@
+package UPF2022SS.KoonsDiarySpring.api.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class ProfileController {
+
+    private final Environment env;
+
+    @GetMapping("/profile")
+    public String profile() {
+        List<String> profiles = Arrays.asList(env.getActiveProfiles());
+//        List<String> realProfiles = Arrays.asList("real1", "real2");
+
+        List<String> testProfiles = Arrays.asList("test");
+        String defaultProfile = profiles.isEmpty()? "default" : profiles.get(0);
+
+        // test 값 반환
+        return profiles.stream()
+                .filter(testProfiles::contains)
+                .findAny()
+                .orElse(defaultProfile);
+    }
+}
