19709a6172f1e5ffc0bc771dd043ba66ae4fc008
ingkoon <ing97220@naver.com>
2022-05-17 01:50:48 +0900

    KDS-54 이미지경로 반환 기능 추가

--- src/main/java/UPF2022SS/KoonsDiarySpring/service/diary/DiaryService.java
+++ src/main/java/UPF2022SS/KoonsDiarySpring/service/diary/DiaryService.java
@@ -3,15 +3,11 @@ package UPF2022SS.KoonsDiarySpring.service.diary;
 
 import UPF2022SS.KoonsDiarySpring.api.dto.diary.*;
 import UPF2022SS.KoonsDiarySpring.api.dto.DefaultResponse;
-import UPF2022SS.KoonsDiarySpring.domain.Diary;
-import UPF2022SS.KoonsDiarySpring.domain.DiaryImage;
 import UPF2022SS.KoonsDiarySpring.domain.User;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
 
-import javax.sound.midi.Patch;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -32,7 +28,7 @@ public interface DiaryService {
 
     public DefaultResponse getDiaryListByLocalDate(User user, LocalDate startDate, LocalDate endDate);
 
-    public ResponseEntity<ByteArrayResource> getDiaryImage(String imagePath);
+    public ResponseEntity<ByteArrayResource> getDiaryImageV1(String imagePath);
     // 감정 분석 api 서비스
 
     // 감정 선택 서비스
