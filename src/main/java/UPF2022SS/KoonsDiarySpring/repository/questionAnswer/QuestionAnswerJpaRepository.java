6d08eda2e8f0bf4447abfc02f2be9f1fba7c924e
InJae Lee <59018852+ingkoon@users.noreply.github.com>
2022-05-19 23:54:26 +0900

    Revert "솜사탕 씻어내기 도메인/리포지토리 개발"
    This reverts commit 7234e519b27b3029bfb6eb8fa336b0c2b32cfe3e.

--- src/main/java/UPF2022SS/KoonsDiarySpring/repository/questionAnswer/QuestionAnswerJpaRepository.java
+++
@@ -1,9 +0,0 @@
-package UPF2022SS.KoonsDiarySpring.repository.questionAnswer;
-
-import UPF2022SS.KoonsDiarySpring.domain.QuestionAnswer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface QuestionAnswerJpaRepository extends JpaRepository<QuestionAnswer, String> {
-}
