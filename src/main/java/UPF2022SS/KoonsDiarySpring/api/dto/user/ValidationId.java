b8e50368b31b70c11f6e1114285858f58f18b763
ingkoon <ing97220@naver.com>
2022-05-23 19:48:33 +0900

    카카오 로그인 DTO 추가

--- src/main/java/UPF2022SS/KoonsDiarySpring/api/dto/user/ValidationId.java
+++ src/main/java/UPF2022SS/KoonsDiarySpring/api/dto/user/ValidationId.java
@@ -3,11 +3,14 @@ package UPF2022SS.KoonsDiarySpring.api.dto.user;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+
 
 public class ValidationId {
 
     @Data
     public static class Request{
+        @NotNull(message = "아이디를 입력해 주세요")
         private String id;
     }
 
