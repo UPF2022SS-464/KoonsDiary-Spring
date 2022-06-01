ebbe603ac6e82c5f2fb3821b024a69bed8b74674
ingkoon <ing97220@naver.com>
2022-06-01 17:13:18 +0900

    솜사탕 질문 포스팅 기능 추가

--- src/main/java/UPF2022SS/KoonsDiarySpring/service/question/questionAnswer/QuestionAnswerService.java
+++ src/main/java/UPF2022SS/KoonsDiarySpring/service/question/questionAnswer/QuestionAnswerService.java
@@ -1,6 +1,5 @@
 package UPF2022SS.KoonsDiarySpring.service.question.questionAnswer;
 
-import UPF2022SS.KoonsDiarySpring.api.dto.DefaultResponse;
 import UPF2022SS.KoonsDiarySpring.domain.Question;
 import UPF2022SS.KoonsDiarySpring.domain.QuestionAnswer;
 import UPF2022SS.KoonsDiarySpring.domain.User;
@@ -8,11 +7,11 @@ import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
-import static UPF2022SS.KoonsDiarySpring.api.dto.question.QuestionRequest.*;
+import static UPF2022SS.KoonsDiarySpring.api.dto.question.Question.*;
 
 @Service
 public interface QuestionAnswerService {
-    public Optional<QuestionAnswer> saveQuestionAnswer(answerRequest answerRequest);
+    public Optional<QuestionAnswer> saveQuestionAnswer(User user, AnswerRequest answerRequest);
 
     public Optional<QuestionAnswer> getQuestionAnswer(User user, Question question);
 //    Optional<QuestionAnswer>
