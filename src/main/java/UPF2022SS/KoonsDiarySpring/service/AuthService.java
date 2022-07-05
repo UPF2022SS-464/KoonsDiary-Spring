1fded097a97b7cb107d7a72f56e2c80f3bf26fb9
ingkoon <ing97220@naver.com>
2022-07-05 23:41:00 +0900

    불필요 dto 제거

--- src/main/java/UPF2022SS/KoonsDiarySpring/service/AuthService.java
+++ src/main/java/UPF2022SS/KoonsDiarySpring/service/AuthService.java
@@ -1,7 +1,5 @@
 package UPF2022SS.KoonsDiarySpring.service;
 
-
-import UPF2022SS.KoonsDiarySpring.api.dto.user.Login;
 import UPF2022SS.KoonsDiarySpring.domain.RefreshToken;
 import UPF2022SS.KoonsDiarySpring.repository.refreshToken.RefreshTokenJpaRepository;
 import UPF2022SS.KoonsDiarySpring.repository.user.UserJpaRepository;
@@ -18,7 +16,6 @@ import org.springframework.transaction.annotation.Transactional;
 import javax.persistence.EntityNotFoundException;
 import java.time.LocalDate;
 
-import static UPF2022SS.KoonsDiarySpring.api.dto.user.SignUp.*;
 
 @Slf4j
 @Service
@@ -35,20 +32,6 @@ public class AuthService {
 
     private final JwtService jwtService;
 
-    //회원가입시 로그인까지 함께 전달
-    public ResponseEntity<Response> signUpLogin(final User user, String refreshTokenDto) {
-        // Access, refresh token을 생성 후 , 메시지 응답과 함께 전달
-        final String accessTokenDto = jwtService.createAccessToken(user.getId());
-
-        Response response = new Response(
-                accessTokenDto,
-                refreshTokenDto,
-                user.getUsername()
-        );
-        return ResponseEntity
-                .ok()
-                .body(response);
-    }
 
     public String createRefreshToken() {
         return jwtService.createRefreshToken();
@@ -75,71 +58,71 @@ public class AuthService {
     만료시 refresh token 재발급
     만료 이전일 경우 access token 발급
      */
-    public ResponseEntity<Object> requestLogin(final Login.Request request, String refreshToken) {
+//    public ResponseEntity<Object> requestLogin(final Login.Request request, String refreshToken) {
+//
+//        final User user = userJpaRepository.findByUsername(request.getUserId()).orElseThrow(EntityNotFoundException::new);
+//
+//        try {
+//            //비밀번호 일치 여부에 대한 부분
+//            if (passwordEncoder.matches(request.getPassword(), user.getPassword())) {
+//                //토큰을 생성 후 , 메시지 응답과 함께 전달
+//                final String accessToken = jwtService.createAccessToken(user.getId());
+//                Login.Response response = new Login.Response(
+//                        user.getId(),
+//                        user.getUsername(),
+//                        accessToken,
+//                        refreshToken
+//                );
+//                return  ResponseEntity
+//                        .status(HttpStatus.OK)
+//                        .body(response);
+//                }
+//                return ResponseEntity
+//                        .status(HttpStatus.UNAUTHORIZED).
+//                        body(ResponseMessage.LOGIN_FAIL);
+//
+//            } catch (Exception e) {
+//            log.error(e.getMessage());
+//            return ResponseEntity
+//                    .status(HttpStatus.UNAUTHORIZED)
+//                    .body(ResponseMessage.LOGIN_FAIL);
+//        }
+//    }
 
-        final User user = userJpaRepository.findByUsername(request.getUserId()).orElseThrow(EntityNotFoundException::new);
-
-        try {
-            //비밀번호 일치 여부에 대한 부분
-            if (passwordEncoder.matches(request.getPassword(), user.getPassword())) {
-                //토큰을 생성 후 , 메시지 응답과 함께 전달
-                final String accessToken = jwtService.createAccessToken(user.getId());
-                Login.Response response = new Login.Response(
-                        user.getId(),
-                        user.getUsername(),
-                        accessToken,
-                        refreshToken
-                );
-                return  ResponseEntity
-                        .status(HttpStatus.OK)
-                        .body(response);
-                }
-                return ResponseEntity
-                        .status(HttpStatus.UNAUTHORIZED).
-                        body(ResponseMessage.LOGIN_FAIL);
-
-            } catch (Exception e) {
-            log.error(e.getMessage());
-            return ResponseEntity
-                    .status(HttpStatus.UNAUTHORIZED)
-                    .body(ResponseMessage.LOGIN_FAIL);
-        }
-    }
-
-    public ResponseEntity<Object> tokenLogin(final String token){
-     try{
-         RefreshToken refreshToken = refreshTokenJpaRepository.findByValue(token);
-
-        if(refreshToken == null){
-            return ResponseEntity
-                    .badRequest().
-                    body(ResponseMessage.BAD_REQUEST);
-        }
-
-         User user = refreshToken.getUser();
-
-         //회원이 존재하지 않을 경우에 대한 응답
-        if (user == null) {
-            return ResponseEntity
-                    .status(HttpStatus.NO_CONTENT)
-                    .body(ResponseMessage.INVALID_USER);
-        }
-
-         checkExpirationDate(user);
-
-         final String accessToken = jwtService.createAccessToken(user.getId());
-
-         Login.Response response = new Login.Response(
-                 user.getId(),
-                 user.getUsername(),
-                 accessToken,
-                 user.getRefreshToken().getValue()
-         );
-         return ResponseEntity.ok().body(response);
-
-     }catch (Exception e){
-        log.error(e.getMessage());
-        return ResponseEntity.internalServerError().body(ResponseMessage.INTERNAL_SERVER_ERROR);
-     }
-    }
+//    public ResponseEntity<Object> tokenLogin(final String token){
+//     try{
+//         RefreshToken refreshToken = refreshTokenJpaRepository.findByValue(token);
+//
+//        if(refreshToken == null){
+//            return ResponseEntity
+//                    .badRequest().
+//                    body(ResponseMessage.BAD_REQUEST);
+//        }
+//
+//         User user = refreshToken.getUser();
+//
+//         //회원이 존재하지 않을 경우에 대한 응답
+//        if (user == null) {
+//            return ResponseEntity
+//                    .status(HttpStatus.NO_CONTENT)
+//                    .body(ResponseMessage.INVALID_USER);
+//        }
+//
+//         checkExpirationDate(user);
+//
+//         final String accessToken = jwtService.createAccessToken(user.getId());
+//
+//         Login.Response response = new Login.Response(
+//                 user.getId(),
+//                 user.getUsername(),
+//                 accessToken,
+//                 user.getRefreshToken().getValue()
+//         );
+//         return ResponseEntity.ok().body(response);
+//
+//     }catch (Exception e){
+//        log.error(e.getMessage());
+//        return ResponseEntity.internalServerError().body(ResponseMessage.INTERNAL_SERVER_ERROR);
+//     }
+//    }
 }
